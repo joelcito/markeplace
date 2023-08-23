85e1f53e15d2ab2e3fa95e8049dae67a3f3614bd
joelcito <jjjoelcito123@gmail.com>
2023-08-23 19:54:48 -0400

    se agego

--- resources/views/tienda/ajaxListado.blade.php
+++ resources/views/tienda/ajaxListado.blade.php
@@ -15,111 +15,81 @@
         </tr>
     </thead>
     <tbody>
-        {{-- @dd($categorias) --}}
         @foreach ($categorias as $c)
-        <tr>
-            <td>{{ $c->idTienda }}</td>
-            <td>
-                <img src="{{ $c->logo }}" alt="">
-            </td>
-            <td>{{ $c->nombre }}</td>
-            <td>{{ $c->nit }}</td>
-            <td>{{ $c->celular }}</td>
-            <td>{{ $c->correo }}</td>
-            <td>
-                @php
-                    dd(
-                        $c,
-                        $persona_id,
-                        $perfil,
-                        $suscripcion
-                    );
-                    $persona_id     = $c->usuario_creacion;
-                    $perfil         = App\Models\Perfil::where('idPersona',$persona_id)->first();
-                    $suscripcion    = App\Models\Suscripcion::where('idPerfil', $perfil->idPerfil)->latest('fecha_creacion')->first();
-                @endphp
-                @if ($suscripcion)
-                    <span>{{ $suscripcion->fecha_inicio }}</span>
-                @endif
-
-            </td>
-            <td>
-                @if ($suscripcion)
-                    <span>{{ $suscripcion->fecha_final }}</span>
-                @endif
-            </td>
-            <td>
-                @if ($suscripcion)
-                    <span >
-                        @php
-                            echo ($suscripcion->tipo_fecha == 1)? 'MENSUAL' : 'ANUAL';
-                            $tipo = ($suscripcion->tipo_fecha == 1)? 1 : 2;
-                            if($perfil->plandepago === 1){
-                                echo " / BASICA";
-                                $plan = 1;
-                            }elseif($perfil->plandepago === 2){
-                                echo " / ESTANDAR";
-                                $plan = 2;
-                            }elseif($perfil->plandepago === 3){
-                                echo " / SUPERIOR";
-                                $plan = 3;
-                            }
-                        @endphp
-                        {{--  {{ ($suscripcion->tipo_fecha == 1)? 'MENSUAL' : 'ANUAL' }} /
-                        @if ($perfil->plandepago === 1)
-                            BASICA
-                        @elseif($perfil->plandepago === 2)
-                            ESTANDAR
-                        @elseif($perfil->plandepago === 3)
-                            SUPERIOR
-                        @endif  --}}
-                    </span>
-                @else
-                    <span >MENSUAL / BASICA</span>
+        @php
+            $persona_id     = $c->usuario_creacion;
+            $perfil         = App\Models\Perfil::where('idPersona',$persona_id)->first();
+        @endphp
+        @if($perfil)
+            <tr>
+                <td>{{ $c->idTienda }}</td>
+                <td>
+                    <img src="{{ $c->logo }}" alt="">
+                </td>
+                <td>{{ $c->nombre }}</td>
+                <td>{{ $c->nit }}</td>
+                <td>{{ $c->celular }}</td>
+                <td>{{ $c->correo }}</td>
+                <td>
                     @php
-                        $tipo = 1;
-                        $plan = 1;
+                        $suscripcion    = App\Models\Suscripcion::where('idPerfil', $perfil->idPerfil)->latest('fecha_creacion')->first();
                     @endphp
-                @endif
-                {{--  @if ($perfil)
-                    <select name="planPago_{{ $c->idTienda }}" id="planPago_{{ $c->idTienda }}" class="form-control" onchange="cambiaPlanPago({{ $c->idTienda }})">
-                        <option {{ $perfil->plandepago === 1? 'selected' : '' }} value="1">Basica</option>
-                        <option {{ $perfil->plandepago === 2? 'selected' : '' }} value="2">Estandar</option>
-                        <option {{ $perfil->plandepago === 3? 'selected' : '' }} value="3">Superior</option>
-                    </select>
-                @endif  --}}
-                {{--  {{ (($perfil->plandepago === 1)? 'BASICA' : '') }}  --}}
-                {{--  <button type="button" onclick="modalCAmbiaSuscripcion()" class="btn btn-success btn-icon btn-sm w-100"><i class="fa fa-dollar"></i></button>  --}}
-            </td>
-            <td>
-                {{-- @if ($c->estado === 1)
-                    <span class="badge badge-light-success fw-dold">Activo</span>
-                @else
-                    <span class="badge badge-light-danger fw-dold">Inactivo</span>
-                @endif --}}
+                    @if ($suscripcion)
+                        <span>{{ $suscripcion->fecha_inicio }}</span>
+                    @endif
 
-                <select name="estadoTienda_" id="estadoTienda_{{$c->idTienda}}" class="form-control" onchange="cambiaEstadoTienda({{ $c->idTienda }})">
-                    <option {{$c->estado === 1? 'selected' : ''}} value="1">Activo</option>
-                    <option {{$c->estado === 2? 'selected' : ''}} value="2">Suspendido</option>
-                </select>
-                <small class="text-success" id="msg_nesaje_{{$c->idTienda}}" style="display: none">Guardado...</small>
-            </td>
-             <td>
-                <button class="btn btn-success btn-icon btn-sm" onclick="editaSuscripcion('{{ $c->idTienda }}', '{{ $c->nombre }}','{{ $tipo }}','{{ $plan }}')"><i class="fa fa-edit"></i></button></button>
-                <button class="btn btn-warning btn-icon btn-sm" onclick="edita('{{ $c->idTienda }}', '{{ $c->nombre }}', '{{ $c->nit }}', '{{ $c->celular }}', '{{ $c->correo }}', '{{ $c->descripcion }}', '{{ $c->estado }}')"><i class="fa fa-edit"></i></button></button>
-                <button class="btn btn-danger btn-icon btn-sm" onclick="elimina('{{ $c->idTienda }}')"><i class="fa fa-trash"></i></button></button>
-            </td>
-        </tr>
+                </td>
+                <td>
+                    @if ($suscripcion)
+                        <span>{{ $suscripcion->fecha_final }}</span>
+                    @endif
+                </td>
+                <td>
+                    @if ($suscripcion)
+                        <span >
+                            @php
+                                echo ($suscripcion->tipo_fecha == 1)? 'MENSUAL' : 'ANUAL';
+                                $tipo = ($suscripcion->tipo_fecha == 1)? 1 : 2;
+                                if($perfil->plandepago === 1){
+                                    echo " / BASICA";
+                                    $plan = 1;
+                                }elseif($perfil->plandepago === 2){
+                                    echo " / ESTANDAR";
+                                    $plan = 2;
+                                }elseif($perfil->plandepago === 3){
+                                    echo " / SUPERIOR";
+                                    $plan = 3;
+                                }
+                            @endphp
+                        </span>
+                    @else
+                        <span >MENSUAL / BASICA</span>
+                        @php
+                            $tipo = 1;
+                            $plan = 1;
+                        @endphp
+                    @endif
+                </td>
+                <td>
+                    <select name="estadoTienda_" id="estadoTienda_{{$c->idTienda}}" class="form-control" onchange="cambiaEstadoTienda({{ $c->idTienda }})">
+                        <option {{$c->estado === 1? 'selected' : ''}} value="1">Activo</option>
+                        <option {{$c->estado === 2? 'selected' : ''}} value="2">Suspendido</option>
+                    </select>
+                    <small class="text-success" id="msg_nesaje_{{$c->idTienda}}" style="display: none">Guardado...</small>
+                </td>
+                <td>
+                    <button class="btn btn-success btn-icon btn-sm" onclick="editaSuscripcion('{{ $c->idTienda }}', '{{ $c->nombre }}','{{ $tipo }}','{{ $plan }}')"><i class="fa fa-dollar"></i></button></button>
+                    <button class="btn btn-warning btn-icon btn-sm" onclick="edita('{{ $c->idTienda }}', '{{ $c->nombre }}', '{{ $c->nit }}', '{{ $c->celular }}', '{{ $c->correo }}', '{{ $c->descripcion }}', '{{ $c->estado }}')"><i class="fa fa-edit"></i></button></button>
+                    <button class="btn btn-danger btn-icon btn-sm" onclick="elimina('{{ $c->idTienda }}')"><i class="fa fa-trash"></i></button></button>
+                </td>
+            </tr>
+        @endif
         @endforeach
     </tbody>
 </table>
 
 <script>
     $('#tabla_categoria').DataTable({
-        {{--  responsive: true,
-        language: {
-            url: '{{ asset('datatableEs.json') }}',
-        },
-        order: [[ 0, "desc" ]]  --}}
+        responsive: true
     });
 </script>
